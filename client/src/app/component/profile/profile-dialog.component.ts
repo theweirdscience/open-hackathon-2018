564cf3f06c7f356c0dbea5f60f68a828911f9146
code0wl <o.lodriguez@gmail.com>
2018-05-26 23:31:10 +0200

    new build

--- client/src/app/component/profile/profile-dialog.component.ts
+++ client/src/app/component/profile/profile-dialog.component.ts
@@ -7,15 +7,7 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 export class ProfileDialogComponent {
 
   constructor(
-    public dialogRef: MatDialog,
     @Inject(MAT_DIALOG_DATA) public data: any) {
-
-  }
-
-  onNoClick(): void {
-
-    this.dialogRef.close();
-
   }
 
 }
