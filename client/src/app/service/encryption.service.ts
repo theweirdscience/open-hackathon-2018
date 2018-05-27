d6a6eb8c409ac4bf72854e24092855133c8ea65c
code0wl <o.lodriguez@gmail.com>
2018-05-27 08:57:26 +0200

    stuff

--- client/src/app/service/encryption.service.ts
+++ client/src/app/service/encryption.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 
 declare const cryptico: any;
 
@@ -11,20 +11,18 @@ export class EncryptionService {
   publicKey: string;
 
   constructor() {
+
     const somePassphrase = 'The Moon is a Harsh Mistress.';
+    this.restoreKeys(somePassphrase);
+
+    console.log(this.privateKey);
+    console.log(this.publicKey);
+
   }
 
   restoreKeys(passphrase: string) {
     this.privateKey = cryptico.generateRSAKey(passphrase, 512);
     this.publicKey = cryptico.publicKeyString(this.privateKey);
-  }
-
-  getPublicKey(): string {
-    return this.publicKey;
-  }
-
-  getPrivateKey(): string {
-    return this.privateKey;
   }
 
   encrypt(data: string, publicKey: string) {
